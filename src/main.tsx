3430531a8d06339699375864cbe2458cb14eea53
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2026-08-20 06:34:37 -0300

    Update: App import; Remove: unused assets

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import "./index.css";
 
-import { App } from "app";
 import { createRoot } from "react-dom/client";
+
+import { App } from "./app";
 
 createRoot(document.getElementById("root")!).render(<App />);
